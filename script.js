3f191c5227a48a47eb7e4ea4ee3bc6675e68a42e
Saulo Mairesse <saulodavi2001@gmail.com>
2022-02-18 22:29:46 -0300

    Building Quizz Screen

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function comparador() { 
+	return Math.random() - 0.5;
+}
+let porcentagemDeAcertos;
+
 // SCRIPT SCREEN LOBBY
 const MAINURL = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
 
@@ -26,24 +31,24 @@ function showQuizzes(quizzes){
                                         <img src="${data[i].image}">
                                         <p>${data[i].title}</p>
                                     </div>`
-                                    console.log(data[i].id)
+                                    // console.log(data[i].id)
     }
 }
 
 // SCRIPT SCREEN IN QUIZZ
-
+let arrayDeRespostasPossiveis = [];
+let identificadorDoQuizz;
 function callSelectedQuizz(identifierQuizz) {
-    const hideHome = document.querySelector('.home');
-    const quezzScreen = document.querySelector('.quezz-screen');
-    hideHome.classList.add('hide-me');
-    quezzScreen.classList.remove('hide-me');
-    
+    const HIDEHOME = document.querySelector('.home');
+    const QUEZZSCREEN = document.querySelector('.quezz-screen');
     const quizz = axios.get(`${MAINURL}/${identifierQuizz}`);
+    identificadorDoQuizz = identifierQuizz;
+    HIDEHOME.classList.add('hide-me');
+    QUEZZSCREEN.classList.remove('hide-me');
     quizz.then(startQuizz);
 }
 
 function startQuizz(resposta){
-    console.log(resposta.data.image);
     let quezzScreenHTML = document.querySelector('.quezz-screen');
     quezzScreenHTML.innerHTML = `<figure class="quezz-indicator"> 
                                     <img src="${resposta.data.image}">
@@ -73,33 +78,77 @@ function startQuizz(resposta){
                                         voltar pra home
                                     </button>
                                 </footer>`
-    
+    colocarQuestions(resposta);                                 
+    }
+
+function colocarQuestions(resposta) {
+
     for(let i = 0; i < resposta.data.questions.length; i++){
-        let arrayQuestionsSpaceHTML = [];
+        let opcoesDeResposta = resposta.data.questions[i].answers;
         let questionsSpaceHTML = document.querySelector('.questions-space');
-        questionsSpaceHTML.innerHTML +=   `<article class="question">
+        questionsSpaceHTML.innerHTML +=   `<article class="question q${i}">
                                                 <div class="title-question">
                                                     <p>${resposta.data.questions[i].title}</p>
                                                 </div>
                                                 <div class="options-space">
-                                                    <div class="option">
-                                                        imagem da opcao
-                                                        <span>legenda da foto</span>
-                                                    </div>
-                                                    <div class="option">
-                                                        imagem da opcao
-                                                        <span>legenda da foto</span>
-                                                    </div>
-                                                    <div class="option">
-                                                        imagem da opcao
-                                                        <span>legenda da foto</span>
-                                                    </div>
-                                                    <div class="option">
-                                                        imagem da opcao
-                                                        <span>legenda da foto</span>
-                                                    </div>
                                                 </div>
                                             </article>`
-                                            console.log(resposta.data.questions[i]);
+        colocarOpcoesDeResposta(opcoesDeResposta,i);
     }
+}
+
+function colocarOpcoesDeResposta(opcoesDeResposta,i){
+    opcoesDeResposta.sort(comparador);
+    arrayDeRespostasPossiveis.push(opcoesDeResposta)
+    let optionsSpace = document.querySelector(`.q${i} .options-space`);
+    for(let c = 0; c < opcoesDeResposta.length; c++){
+        optionsSpace.innerHTML += `<div class="option opt${c}" onclick="selectOption(${i}, ${c})">
+                                        <img src="${opcoesDeResposta[c].image}">
+                                        <p>${opcoesDeResposta[c].text}</p>
+                                        <span class="${opcoesDeResposta[c].isCorrectAnswer} hide-me"></span>
+                                    </div>`
+    }
+}
+
+function selectOption(questaoDaOpcao,opcaoSelecionada){
+    let a = document.querySelector(`.q${questaoDaOpcao} .opt${opcaoSelecionada}`);
+    a.classList.add('selecionada');
+    for(let cont = 0; cont < document.querySelectorAll(`.q${questaoDaOpcao} .option`).length; cont++){
+        let b = document.querySelector(`.q${questaoDaOpcao} .opt${cont}`);
+        let c = document.querySelector(`.q${questaoDaOpcao} .opt${cont}`);
+        c.setAttribute('onclick', '');
+        if(document.querySelector(`.q${questaoDaOpcao} .opt${cont}`) != a){
+            b.classList.add('opcao-nao-selecionada');
+        }
+        if(!document.querySelector(`.q${questaoDaOpcao} .opt${cont} .true`)){
+            b.classList.add('false');
+        }
+        else{
+            b.classList.add('true');
+        }
+    }
+    if(document.querySelector(`.q${questaoDaOpcao + 1}`)){
+        setTimeout(function(){
+            document.querySelector(`.q${questaoDaOpcao + 1}`).scrollIntoView({block: "center"});},500);
+    }
+    else{
+        calcularAcertos();
+    }
+}
+
+function calcularAcertos(){
+    let a = document.querySelectorAll('.question').length
+    let b = document.querySelectorAll('.selecionada .true').length
+    porcentagemDeAcertos = parseInt(b*100)/a;
+    rechamandoOQuizz();
+}
+
+function rechamandoOQuizz(){
+    const quizz = axios.get(`${MAINURL}/${identificadorDoQuizz}`);
+    quizz.then(mostrarLeveis);
+}
+
+//iniciando a tela de resultado. Parei aqui
+function mostrarLeveis(resposta){
+    console.log(resposta.data.levels)
 }
